54f0c3a9ffb62be7b7f0fe33b7b4a756053d7c98
Ed Hewell <ghewelljr@gmail.com>
2013-09-20 22:09:02 -0700

    Refactored unit test macros to only call functions once

--- MBATool/unit_test.h
+++ MBATool/unit_test.h
@@ -11,87 +11,93 @@ static int  __unit_tests_failed__ = 0;
 
 #define assert(message, test) do { if (!(test)) return message; } while (0)
 
-#define assertEqualsDbl( expected, actual ) \
-do \
-{ \
-     if ( (expected) != (actual) ) \
-     { \
-          sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected: %f, actual: %f\033[0m", #actual, (expected), (actual) ); \
-          return __unit_test_msgbuf__; \
-     } \
-} while ( 0 )
+#define assertEqualsDbl( E, A )                                         \
+     do                                                                 \
+     {                                                                  \
+          float __expected__ = (E);                                     \
+          float __actual__   = (A);                                     \
+          if ( __expected__ != __actual__ )                             \
+          {                                                             \
+               sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected: %f, actual: %f\033[0m", #A, __expected__, __actual__ ); \
+               return __unit_test_msgbuf__;                             \
+          }                                                             \
+     } while ( 0 )
 
-#define assertEqualsInt( expected, actual ) \
-do \
-{ \
-     if ( (expected) != (actual) ) \
-     { \
-          sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected: %d, actual: %d\033[0m", #actual, (expected), (actual) ); \
-          return __unit_test_msgbuf__; \
-     } \
-} while ( 0 )
+#define assertEqualsInt( E, A )                                         \
+     do                                                                 \
+     {                                                                  \
+          int __expected__ = (E);                                       \
+          int __actual__   = (A);                                       \
+          if ( __expected__ != __actual__ )                             \
+          {                                                             \
+               sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected: %d, actual: %d\033[0m", #A, __expected__, __actual__ ); \
+               return __unit_test_msgbuf__;                             \
+          }                                                             \
+     } while ( 0 )
 
-#define assertEqualsStr( expected, actual ) \
-do \
-{ \
-     if ( strcmp( (expected), (actual) ) != 0 ) \
-     { \
-          sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected: %s, actual: %s\033[0m", #actual, (expected), (actual) ); \
-          return __unit_test_msgbuf__; \
-     } \
-} while ( 0 )
+#define assertEqualsStr( E, A )                                         \
+     do                                                                 \
+     {                                                                  \
+          char *__expected__ = (E);                                     \
+          char *__actual__   = (A);                                     \
+          if ( strcmp( __expected__, __actual__ ) != 0 )                \
+          {                                                             \
+               sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected: %s, actual: %s\033[0m", #A, __expected__, __actual__ ); \
+               return __unit_test_msgbuf__;                             \
+          }                                                             \
+     } while ( 0 )
 
-#define assertNotNull( actual ) \
-do \
-{ \
-     if ( (actual) == NULL ) \
-     { \
-          sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected to not be NULL but was NULL\033[0m", #actual ); \
-          return __unit_test_msgbuf__; \
-     } \
-} while ( 0 )
+#define assertNotNull( actual )                                    \
+     do                                                            \
+     {                                                             \
+          if ( (actual) == NULL )                                  \
+          {                                                        \
+               sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected to not be NULL but was NULL\033[0m", #actual ); \
+               return __unit_test_msgbuf__;                        \
+          }                                                        \
+     } while ( 0 )
 
-#define assertNull( actual ) \
-do \
-{ \
-     if ( (actual) != NULL ) \
-     { \
-          sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected to be NULL but was not\033[0m", #actual ); \
-          return __unit_test_msgbuf__; \
-     } \
-} while ( 0 )
+#define assertNull( actual )                                       \
+     do                                                            \
+     {                                                             \
+          if ( (actual) != NULL )                                  \
+          {                                                        \
+               sprintf( __unit_test_msgbuf__, "\033[0;31mFailed: %s; expected to be NULL but was not\033[0m", #actual ); \
+               return __unit_test_msgbuf__;                        \
+          }                                                        \
+     } while ( 0 )
 
 #define assertEquals assertEqualsInt
 
-#define run_test(test, callback) \
-do \
-{ \
-     printf( "Running %s ", #test ); \
-     char *message = test(); \
-     __unit_tests_run__++; \
-     if (message) \
-     { \
-          __unit_tests_failed__++; \
-          printf( "\033[0;31mFAILED\033[0m\n" ); \
-          printf( "%s\n", message ); \
-          if ( callback != NULL ) callback(); \
-          printf( "\n" ); \
-     } \
-     else \
-     { \
-          __unit_tests_passed__++; \
-          printf( "\033[0;32mPASSED\033[0m\n" ); \
-     } \
-} while ( 0 )
+#define run_test(test, callback)                           \
+     do                                                    \
+     {                                                     \
+          printf( "Running %s ", #test );                  \
+          char *message = test();                          \
+          __unit_tests_run__++;                            \
+          if (message)                                     \
+          {                                                \
+               __unit_tests_failed__++;                    \
+               printf( "\033[0;31mFAILED\033[0m\n" );      \
+               printf( "%s\n", message );                  \
+               if ( callback != NULL ) callback();         \
+               printf( "\n" );                             \
+          }                                                \
+          else                                             \
+          {                                                \
+               __unit_tests_passed__++;                    \
+               printf( "\033[0;32mPASSED\033[0m\n" );      \
+          }                                                \
+     } while ( 0 )
 
 
-#define show_test_results() \
-do \
-{ \
-     if ( __unit_tests_failed__ > 0 ) printf( "\033[0;31m" ); else printf( "\033[0;32m" ); \
-     printf( "Tests Run: %d, Passed: %d, Failed: %d\n", __unit_tests_run__, __unit_tests_passed__, __unit_tests_failed__ ); \
-     printf( "\033[0m" ); \
-} while ( 0 )
+#define show_test_results()                                        \
+     do                                                            \
+     {                                                             \
+          if ( __unit_tests_failed__ > 0 ) printf( "\033[0;31m" ); else printf( "\033[0;32m" ); \
+          printf( "Tests Run: %d, Passed: %d, Failed: %d\n", __unit_tests_run__, __unit_tests_passed__, __unit_tests_failed__ ); \
+          printf( "\033[0m" );                                     \
+     } while ( 0 )
 
 #define tests_pass() (__unit_tests_failed__ == 0)
 
