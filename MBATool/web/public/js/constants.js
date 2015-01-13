bb0d58a0f7ce2bf0199dfe7c4286ecf424ad82ab
Ed Hewell <ghewelljr@gmail.com>
2015-01-12 22:08:29 -0800

    Setting constants back to the correct values

--- MBATool/web/public/js/constants.js
+++ MBATool/web/public/js/constants.js
@@ -3,10 +3,10 @@ define([], function() {
     var Constants = {
         TEAM_MAX_PITCHERS:          10,
         TEAM_MAX_BATTERS:           15,
-        ROOKIE_DRAFT_MAX_PITCHERS:  10,
-        ROOKIE_DRAFT_MAX_BATTERS:   15,
-        ROSTER_CUT_TARGET_PITCHERS:  9,
-        ROSTER_CUT_TARGET_BATTERS:  14
+        ROOKIE_DRAFT_MAX_PITCHERS:   4,
+        ROOKIE_DRAFT_MAX_BATTERS:    6,
+        ROSTER_CUT_TARGET_PITCHERS:  3,
+        ROSTER_CUT_TARGET_BATTERS:   5
     };
 
     return Constants;
