6554e6a891723188f58468641e937825fe7a9994
Ed Hewell <ghewelljr@gmail.com>
2015-08-31 18:40:22 -0700

    Archive boxscores will only check the number of files if regular season

--- archive_boxscores.sh
+++ archive_boxscores.sh
@@ -14,10 +14,6 @@ if [[ "${BOXSCORES[0]}" == "$FILE_MATCHER" ]]
 then
     echo "No boxscore files. Nothing to do here."
     exit
-elif [[ ${#BOXSCORES[@]} -ne 16 ]]
-then
-    echo "Incorrect number of files: ${#BOXSCORES[@]}."
-    exit
 fi
 
 GAMEDAY=$((10#${BOXSCORES[0]:1:3}))
@@ -30,6 +26,15 @@ then
 fi
 
 echo "Processing Day $GAMEDAY into Month $MONTH..."
+
+if [[ $GAMEDAY -le 152 ]]
+then
+    if [[ ${#BOXSCORES[@]} -ne 16 ]]
+    then
+        echo "Incorrect number of boxscore files: ${#BOXSCORES[@]}."
+        exit
+    fi
+fi
 
 if [[ ! -d "M$MONTH" ]]
 then
