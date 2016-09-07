aa0f7eeb90fe7b76f8493c37cc7b1c05cf66d891
Ed Hewell <ghewelljr@gmail.com>
2016-09-06 22:20:25 -0700

    Making averages look better in leaderboards

--- TopPlayers.rb
+++ TopPlayers.rb
@@ -42,11 +42,19 @@ class Stats
   end
 
   def to_s
+    sort_key = get_sort_key
+    sort_key = @avgs.include?(@sort_key) ? display_avg(sort_key) : sort_key
+
     if @season.nil?
-      sprintf "%-2s %-20s %-15s #{@format}", @pos, @name, @team, get_sort_key
+      sprintf "%-2s %-20s %-15s #{@format}", @pos, @name, @team, sort_key
     else
-      sprintf "%-2s %-20s S%02d  %-15s #{@format}", @pos, @name, @season, @team, get_sort_key
+      sprintf "%-2s %-20s S%02d  %-15s #{@format}", @pos, @name, @season, @team, sort_key
     end
+  end
+
+  def display_avg(average)
+    avg = sprintf "%5.3f", average
+    avg.gsub /^0\./, ' .'
   end
 
 end
@@ -73,16 +81,10 @@ end
 
 class Pitcher < Stats
   attr_reader :name, :wins, :losses, :era, :games, :saves, :innings
-  attr_reader :hits, :earned_runs, :home_runs, :walks, :strike_outs, :vsba, :win_pct2
-  attr_reader :inn_per_game, :whip, :so_per_9, :hr_per_9, :win_pct, :efficiency, :eff_per_9
+  attr_reader :hits, :earned_runs, :home_runs, :walks, :strike_outs, :vsba, :win_pct
+  attr_reader :inn_per_game, :whip, :so_per_9, :hr_per_9, :wl_ratio, :efficiency, :eff_per_9
 
   def initialize( team, pitcher, style=:simulated )
-    adj_innings = pitcher[:stats][style][:innings] + (pitcher[:stats][style][:outs] / 3)
-    adj_outs    = pitcher[:stats][style][:outs] % 3
-    est_at_bats = (adj_innings * 3) + adj_outs + pitcher[:stats][style][:hits]
-
-    f_innings = adj_innings.to_f + (adj_outs.to_f / 3.0)
-
     @name         = "#{pitcher[:last_name]}, #{pitcher[:first_name]}"
     @team         = team
     @season       = pitcher[:stats][style][:season]
@@ -91,22 +93,31 @@ class Pitcher < Stats
     @losses       = pitcher[:stats][style][:losses]
     @games        = pitcher[:stats][style][:games]
     @saves        = pitcher[:stats][style][:saves]
-    @innings      = "#{adj_innings}.#{adj_outs}"
     @hits         = pitcher[:stats][style][:hits]
     @earned_runs  = pitcher[:stats][style][:earned_runs]
     @home_runs    = pitcher[:stats][style][:home_runs]
     @walks        = pitcher[:stats][style][:walks]
     @strike_outs  = pitcher[:stats][style][:strike_outs]
-    @era          = (f_innings > 0) ? pitcher[:stats][style][:earned_runs].to_f / f_innings * 9.0 : 0
-    @vsba         = (est_at_bats > 0) ? pitcher[:stats][style][:hits].to_f / est_at_bats.to_f : 0
-    @inn_per_game = (pitcher[:stats][style][:games] > 0) ? f_innings / pitcher[:stats][style][:games].to_f : 0
-    @whip         = (f_innings > 0) ? (pitcher[:stats][style][:walks].to_f + pitcher[:stats][style][:hits].to_f) / f_innings : 0
-    @so_per_9     = (f_innings > 0) ? pitcher[:stats][style][:strike_outs].to_f / f_innings * 9.0 : 0
-    @hr_per_9     = (f_innings > 0) ? pitcher[:stats][style][:home_runs].to_f / f_innings * 9.0 : 0
-    @win_pct      = (@wins + @losses) > 0 ? @wins.to_f / (@wins + @losses) : 0
-    @win_pct2     = (@games > 0) ? @wins.to_f / @games : 0
+
+    adj_innings = pitcher[:stats][style][:innings] + (pitcher[:stats][style][:outs] / 3)
+    adj_outs    = pitcher[:stats][style][:outs] % 3
+    est_at_bats = (adj_innings * 3) + adj_outs + @hits
+
+    f_innings = adj_innings.to_f + (adj_outs.to_f / 3.0)
+
+    @innings      = "#{adj_innings}.#{adj_outs}"
+    @wl_ratio     = (@wins + @losses) > 0 ? @wins.to_f / (@wins + @losses) : 0
+    @win_pct      = (@games > 0) ? @wins.to_f / @games : 0
+    @era          = (f_innings > 0) ? @earned_runs.to_f / f_innings * 9.0 : 0
+    @vsba         = (est_at_bats > 0) ? @hits.to_f / est_at_bats.to_f : 0
+    @inn_per_game = (@games > 0) ? f_innings / @games.to_f : 0
+    @whip         = (f_innings > 0) ? (@walks.to_f + @hits.to_f) / f_innings : 0
+    @so_per_9     = (f_innings > 0) ? @strike_outs.to_f / f_innings * 9.0 : 0
+    @hr_per_9     = (f_innings > 0) ? @home_runs.to_f / f_innings * 9.0 : 0
     @efficiency   = (adj_innings - @hits) + (@strike_outs - @hits)
     @eff_per_9    = (f_innings > 0) ? @efficiency.to_f / f_innings * 9.0 : 0
+
+    @avgs = [:wl_ratio, :win_pct, :vsba]
   end
 
 end
@@ -117,12 +128,6 @@ class Batter < Stats
   attr_reader :steals, :errors, :slugging, :obp, :soa, :rpg
 
   def initialize( team, batter, style=:simulated )
-
-    appearances = batter[:stats][style][:at_bats] + batter[:stats][style][:walks]
-    singles     = batter[:stats][style][:hits] - (batter[:stats][style][:doubles] + batter[:stats][style][:triples] + batter[:stats][style][:home_runs])
-    bases       = singles + (batter[:stats][style][:doubles] * 2) + (batter[:stats][style][:triples] * 3) + (batter[:stats][style][:home_runs] * 4)
-    production  = batter[:stats][style][:runs] + batter[:stats][style][:runs_batted_in] - batter[:stats][style][:home_runs]
-
     @name        = "#{batter[:last_name]}, #{batter[:first_name]}"
     @team        = team
     @season      = batter[:stats][style][:season]
@@ -139,11 +144,19 @@ class Batter < Stats
     @strike_outs = batter[:stats][style][:strike_outs]
     @steals      = batter[:stats][style][:steals]
     @errors      = batter[:stats][style][:errors]
-    @average     = (batter[:stats][style][:at_bats] > 0) ? batter[:stats][style][:hits].to_f / batter[:stats][style][:at_bats].to_f : 0
-    @slugging    = (batter[:stats][style][:at_bats] > 0) ? bases.to_f / batter[:stats][style][:at_bats].to_f : 0
-    @obp         = (batter[:stats][style][:at_bats] > 0) ? (batter[:stats][style][:hits].to_f + batter[:stats][style][:walks].to_f) / appearances.to_f : 0
-    @soa         = (batter[:stats][style][:at_bats] > 0) ? batter[:stats][style][:strike_outs].to_f / batter[:stats][style][:at_bats].to_f : 0
-    @rpg         = (batter[:stats][style][:games] > 0) ? production / batter[:stats][style][:games].to_f : 0
+
+    appearances = @at_bats + @walks
+    singles     = @hits - (@doubles + @triples + @home_runs)
+    bases       = singles + (@doubles * 2) + (@triples * 3) + (@home_runs * 4)
+    production  = @runs + @rbi - @home_runs
+
+    @average     = (@at_bats > 0) ? @hits.to_f / @at_bats.to_f : 0
+    @slugging    = (@at_bats > 0) ? bases.to_f / @at_bats.to_f : 0
+    @obp         = (@at_bats > 0) ? (@hits.to_f + @walks.to_f) / appearances.to_f : 0
+    @soa         = (@at_bats > 0) ? @strike_outs.to_f / @at_bats.to_f : 0
+    @rpg         = (@games > 0) ? production / @games.to_f : 0
+
+    @avgs = [:average, :obp, :soa, :slugging]
   end
 
 end
@@ -297,20 +310,20 @@ end
 @categories = {
   'pitching'      => {  'label' => "Pitching Leaders",     'type' => 'pitcher',     'class'  => Pitcher,
     'stats'       => [{ 'label' => "Wins",                 'stat' => :wins,         'format' => '%2d',    'direction' => :descending },
-                      { 'label' => "Win/Loss Ratio",       'stat' => :win_pct,      'format' => '%5.3f',  'direction' => :descending },
-                      { 'label' => "Win Percentage",       'stat' => :win_pct2,     'format' => '%5.3f',  'direction' => :descending },
+                      { 'label' => "Win/Loss Ratio",       'stat' => :wl_ratio,     'format' => '%s',     'direction' => :descending },
+                      { 'label' => "Win Percentage",       'stat' => :win_pct,      'format' => '%s',     'direction' => :descending },
                       { 'label' => "Earned Run Average",   'stat' => :era,          'format' => '%5.2f',  'direction' => :ascending  },
-                      { 'label' => "vs. Batting Average",  'stat' => :vsba,         'format' => '%5.3f',  'direction' => :ascending  },
+                      { 'label' => "vs. Batting Average",  'stat' => :vsba,         'format' => '%s',     'direction' => :ascending  },
                       { 'label' => "Saves",                'stat' => :saves,        'format' => '%3d',    'direction' => :descending },
-                      { 'label' => "Strike Outs",          'stat' => :strike_outs,  'format' => '%3d',    'direction' => :descending },
                       { 'label' => "Innings Per Game",     'stat' => :inn_per_game, 'format' => '%5.2f',  'direction' => :descending },
-                      { 'label' => "Walks & Hits Per Inn", 'stat' => :whip,         'format' => '%5.2f',  'direction' => :ascending  },
+                      { 'label' => "Strike Outs",          'stat' => :strike_outs,  'format' => '%3d',    'direction' => :descending },
                       { 'label' => "Strikeouts Per 9 Inn", 'stat' => :so_per_9,     'format' => '%5.2f',  'direction' => :descending },
+                      { 'label' => "Walks & Hits Per Inn", 'stat' => :whip,         'format' => '%6.3f',  'direction' => :ascending  },
                       { 'label' => "Home Runs Per 9 Inn",  'stat' => :hr_per_9,     'format' => '%5.2f',  'direction' => :ascending  },
                       { 'label' => "Efficiency",           'stat' => :eff_per_9,    'format' => '%+5.2f', 'direction' => :descending }]},
 
   'batting '      => {  'label' => "Hitting Leaders",      'type' => 'batter',      'class'  => Batter,
-    'stats'       => [{ 'label' => "Batting Average",      'stat' => :average,      'format' => '%5.3f',  'direction' => :descending },
+    'stats'       => [{ 'label' => "Batting Average",      'stat' => :average,      'format' => '%s',     'direction' => :descending },
                       { 'label' => "Doubles",              'stat' => :doubles,      'format' => '%3d',    'direction' => :descending },
                       { 'label' => "Triples",              'stat' => :triples,      'format' => '%3d',    'direction' => :descending },
                       { 'label' => "Home Runs",            'stat' => :home_runs,    'format' => '%3d',    'direction' => :descending },
@@ -319,8 +332,8 @@ end
                       { 'label' => "Stolen Bases",         'stat' => :steals,       'format' => '%3d',    'direction' => :descending },
                       { 'label' => "Walks",                'stat' => :walks,        'format' => '%3d',    'direction' => :descending },
                       { 'label' => "Errors",               'stat' => :errors,       'format' => '%3d',    'direction' => :ascending  },
-                      { 'label' => "Slugging Average",     'stat' => :slugging,     'format' => '%5.3f',  'direction' => :descending },
-                      { 'label' => "On Base Percentage",   'stat' => :obp,          'format' => '%5.3f',  'direction' => :descending },
-                      { 'label' => "Strike Out Average",   'stat' => :soa,          'format' => '%5.3f',  'direction' => :ascending  },
+                      { 'label' => "Slugging Average",     'stat' => :slugging,     'format' => '%s',     'direction' => :descending },
+                      { 'label' => "On Base Percentage",   'stat' => :obp,          'format' => '%s',     'direction' => :descending },
+                      { 'label' => "Strike Out Average",   'stat' => :soa,          'format' => '%s',     'direction' => :ascending  },
                       { 'label' => "Runs Per Game",        'stat' => :rpg,          'format' => '%4.2f',  'direction' => :descending }]}
 }
