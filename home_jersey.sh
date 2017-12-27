572d7029821dd7f6f25875b25c4aa04451a61367
Ed Hewell <ghewelljr@gmail.com>
2017-12-27 12:26:47 -0800

    Adding scripts to launch image view of team jersey based on team name

---
+++ home_jersey.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+#
+TEAM=$1
+BASE_PATH=$( cd ${BASH_SOURCE%/*}; pwd -P )
+IMAGE_FILE="$BASE_PATH/jerseys/${TEAM}_Home_Front.png"
+
+if [[ ! -f "$IMAGE_FILE" ]];
+then
+    echo "Jersey image $IMAGE_FILE does not exist!";
+    exit;
+fi
+
+viewnior $IMAGE_FILE &
